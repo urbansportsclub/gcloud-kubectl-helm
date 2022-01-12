17ad8416765a43e2f31bee579b151534b3914c27
roman-usc <roman.soldatov@urbansportsclub.com>
2022-01-12 17:52:22 +0100

    upgrade components: see https://github.com/kiwigrid/gcloud-kubectl-helm/pull/25

--- helm-init.sh
+++ helm-init.sh
@@ -3,10 +3,15 @@
 set -e
 set -x
 
-if helm version --client | grep -q 'SemVer:"v2';then 
+if helm version --client | grep -q 'SemVer:"v2';then
     helm init --client-only
-else 
-    helm repo add stable https://kubernetes-charts.storage.googleapis.com
+    helm plugin install https://github.com/hayorov/helm-gcs.git --version 0.2.2
+    helm plugin install https://github.com/databus23/helm-diff --version 3.1.3
+    helm plugin list
+else
+    helm repo add stable https://charts.helm.sh/stable
+    helm plugin install https://github.com/jkroepke/helm-secrets --version v3.9.1
+    helm plugin install https://github.com/hayorov/helm-gcs.git --version 0.3.18
+    helm plugin install https://github.com/databus23/helm-diff --version 3.1.3
+    helm plugin list
 fi
-
-helm plugin install https://github.com/futuresimple/helm-secrets.git
